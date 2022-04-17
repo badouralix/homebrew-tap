6dfe8845049176512f70bec63417217f55861b6f
Ayaz Badouraly <badouralix@users.noreply.github.com>
2022-04-17 02:32:28 +0200

    Use versioned kubekeeper

--- Formula/kubekeeper.rb
+++ Formula/kubekeeper.rb
@@ -1,6 +1,8 @@
 class Kubekeeper < Formula
   desc "Safeguard for kubectl"
   homepage "https://github.com/badouralix/kubekeeper"
+  url "https://github.com/badouralix/kubekeeper/archive/v1.3.0.tar.gz"
+  sha256 "932366a8f56f533df1042eedfcf3bf2e0fa5a67abbf4172c5d65e81fa45d9a57"
   license "MIT"
   head "https://github.com/badouralix/kubekeeper.git", branch: "main"
 
