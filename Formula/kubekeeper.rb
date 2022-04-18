abd617a0471fde3d1751605d764abd32c176701d
Ayaz Badouraly <badouralix@users.noreply.github.com>
2022-04-19 00:08:53 +0200

    kubekeeper 2.0.1

--- Formula/kubekeeper.rb
+++ Formula/kubekeeper.rb
@@ -1,8 +1,8 @@
 class Kubekeeper < Formula
   desc "Safeguard for kubectl"
   homepage "https://github.com/badouralix/kubekeeper"
-  url "https://github.com/badouralix/kubekeeper/archive/v2.0.0.tar.gz"
-  sha256 "a6fdea596f0db64f50bfe5c72cd3761adf83cfa719b509a93746329820d89b7f"
+  url "https://github.com/badouralix/kubekeeper/archive/v2.0.1.tar.gz"
+  sha256 "a9962f80441aa7658e40f734ee90e06e7f729d9da11b192bb083d92b321b170e"
   license "MIT"
   head "https://github.com/badouralix/kubekeeper.git", branch: "main"
 
