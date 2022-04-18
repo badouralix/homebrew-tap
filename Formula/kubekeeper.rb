1e8b9c523e434608b7dacc9931741e51f1875366
Ayaz Badouraly <badouralix@users.noreply.github.com>
2022-04-18 18:46:22 +0200

    Bump kubekeeper version to v2.0.0

--- Formula/kubekeeper.rb
+++ Formula/kubekeeper.rb
@@ -1,16 +1,17 @@
 class Kubekeeper < Formula
   desc "Safeguard for kubectl"
   homepage "https://github.com/badouralix/kubekeeper"
-  url "https://github.com/badouralix/kubekeeper/archive/v1.3.0.tar.gz"
-  sha256 "932366a8f56f533df1042eedfcf3bf2e0fa5a67abbf4172c5d65e81fa45d9a57"
+  url "https://github.com/badouralix/kubekeeper/archive/v2.0.0.tar.gz"
+  sha256 "a6fdea596f0db64f50bfe5c72cd3761adf83cfa719b509a93746329820d89b7f"
   license "MIT"
   head "https://github.com/badouralix/kubekeeper.git", branch: "main"
 
-  depends_on "fzf"
   depends_on "kubernetes-cli"
+  depends_on "rust" => :build
 
   def install
-    bin.install "src/kubekeeper"
+    system "cargo", "build", "--release", "--bin", "kubekeeper"
+    bin.install "target/release/kubekeeper"
   end
 
   test do
